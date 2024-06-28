0a9a3b79c474b68da0494950ccfa442a4c16068b
Iwo <iwo.smura@gmail.com>
2024-06-28 11:48:40 +0200

    improve button style

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import food from '../assets/restauranfood.jpg'
+import {Link} from 'react-router-dom'
 
 const Hero = () => {
   return (
@@ -8,7 +9,7 @@ const Hero = () => {
         <h1>Little Lemon</h1>
         <h3>Chicago</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <button className="btn">Reserve a Table</button>
+        <button className="btn"><Link to="/reservations" id='link'>Reserve a Table</Link></button>
       </div>
       <img src={food} alt="Delicious food" />
     </section>
